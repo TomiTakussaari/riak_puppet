82150b6e5260642c3c9026d4ed62f46df1df5d63
Tomi Takussaari <tomi.takussaari@gmail.com>
2013-03-24 22:35:27 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,11 @@
 node "riak1", "riak2", "riak3" {
 	exec { "purge default firewall":
-        command => "/sbin/iptables --flush",
-        user    => 'root',
-    }
+                command => "/sbin/iptables --flush",
+                user    => 'root',
+        }
   	if $::fqdn == "riak1.vagrant.local" {
-    	class { "riak": riak_control => true}
+    	        class { "riak": riak_control => true}
   	} else {
-    	class { "riak": join_ip => "10.0.3.10" }
+    	        class { "riak": join_ip => "10.0.3.10" }
   	}
 }
