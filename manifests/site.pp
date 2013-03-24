682b5f8aa5e4710a41b53fa9f82004181628d5b0
Tomi Takussaari <tomi.takussaari@elisa.fi>
2013-03-24 22:29:11 +0200

    Purge default firewall so nodes can talk to each other

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,11 @@
 node "riak1", "riak2", "riak3" {
-  if $::fqdn == "riak1.vagrant.local" {
-    class { "riak": riak_control => true}
-  } else {
-    class { "riak": join_ip => "10.0.3.10" }
-  }
+	exec { "purge default firewall":
+        command => "/sbin/iptables --flush",
+        user    => 'root',
+    }
+  	if $::fqdn == "riak1.vagrant.local" {
+    	class { "riak": riak_control => true}
+  	} else {
+    	class { "riak": join_ip => "10.0.3.10" }
+  	}
 }
